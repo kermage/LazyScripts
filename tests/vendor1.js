69e6e8b84e89a419e9baf6ebb0c0f567a0792dc1
kermage <genealyson.torcende@gmail.com>
2025-03-22 23:26:57 +0800

    commit test scripts

---
+++ tests/vendor1.js
@@ -0,0 +1,14 @@
+function customLogger( event ) {
+	console.log( 'Vendor #1', 'Triggered', event.type, event.currentTarget.toString(), event );
+}
+
+console.log( document.currentScript.src, 'LOADED!' );
+
+document.addEventListener( 'DOMContentLoaded', customLogger );
+window.addEventListener( 'DOMContentLoaded', customLogger );
+window.addEventListener( 'load', customLogger );
+window.addEventListener( 'pageshow', customLogger );
+
+window.onpageshow = function(event) {
+	console.log('Vendor #1', 'pageshow', event)
+}
---
+++ tests/vendor2.js
@@ -0,0 +1,14 @@
+function customLogger( event ) {
+	console.log( 'Vendor #2', 'Triggered', event.type, event.currentTarget.toString(), event );
+}
+
+console.log( document.currentScript.src, 'LOADED!' );
+
+document.addEventListener( 'DOMContentLoaded', customLogger );
+window.addEventListener( 'DOMContentLoaded', customLogger );
+window.addEventListener( 'load', customLogger );
+window.addEventListener( 'pageshow', customLogger );
+
+window.onpageshow = function(event) {
+	console.log('Vendor #2', 'pageshow', event)
+}
